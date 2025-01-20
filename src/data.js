8876f65295275df567a47a5e64978be685edd940
mspr <mspr9773@gmail.com>
2025-01-20 19:12:54 +0530

    changes done

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "We have deploying in this application into AWS with the help of Ansible",
+    title: "We have deploying in this application into AWS with the help of Ansible is working now",
     desc: "This is now in new commit",
     bg: "green",
   },
