787137154a8e2526c66031b29399dae1281c6f79
mspr <mspr9773@gmail.com>
2025-01-22 18:37:38 +0530

    file edited

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: "We have deploying in this application using Jenkins",
+    title: "We have deploying in this application using deployserver",
     desc: "This is now in new commit",
     bg: "green",
   },
