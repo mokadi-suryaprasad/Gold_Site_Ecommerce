ba6af4a1b0f2ff1d4c3bf1b8f23412b0a06da067
mspr <mspr9773@gmail.com>
2025-01-25 18:22:24 +0530

    uodated the data.js file

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: " Deploying with 3 servers",
+    title: " We are deploying with helm charts",
     desc: "This is now in new commit",
     bg: "green",
   },
