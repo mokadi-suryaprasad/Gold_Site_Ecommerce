17ebb978a3e2425b094fa3aefebcbe4f5039b5f4
mspr <mspr9773@gmail.com>
2025-01-25 19:04:24 +0530

    uodated the data.js file

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: " We are deploying with helm charts using with git commit idss ",
+    title: " Surya deploying with helm charts using with git commit ids ",
     desc: "This is now in new commit",
     bg: "green",
   },
