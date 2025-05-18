ea7b73188932bf17b86102e880872240b4d0b7cf
Mokadi Surya Prasad <msuryaprasad11@gmail.com>
2025-05-18 11:04:33 +0530

    Modified main page name

--- src/data.js
+++ src/data.js
@@ -2,7 +2,7 @@ export const sliderItems = [
   {
     id: "1",
     img: "assets/braclet/main.jfif",
-    title: " Deploying with 3 servers",
+    title: " Deploying with build server",
     desc: "This is now in new commit",
     bg: "green",
   },
